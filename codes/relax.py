b76b50ffdba6afa6ec9b7f9626c51f37daf01ac9
armmorin <armoma@dtu.dk>
2024-12-16 12:34:48 +0100

    Running the strained p3 calculations with the unit cell

--- codes/relax.py
+++ codes/relax.py
@@ -1,5 +1,6 @@
 from os import environ
 from rich.console import Console
+from ase.optimize import FIRE
 from typing import Optional, Tuple
 from ase import Atoms
 from ase.constraints import FixAtoms
@@ -48,55 +49,83 @@ def main(rattle_std: float=0.03,
     direc: Path = home / f"relaxations/{name}"
     direc.mkdir(parents=True, exist_ok=True)
     c.log(f"Working in {direc}")
-    
+    direc_string = direc.as_posix()
+
     # Check if the atoms object is a supercell.
-    lets_run = True
-    if len(atoms) == 32:
+    #lets_run = True
+    #if len(atoms) == 32:
         # Generate the supercell
-        c.log(f"Generating supercell for {name}")
+        #c.log(f"Generating supercell for {name}")
         #atoms = atoms.repeat([2, 2, 1])
-        cn = FixAtoms([31])
-        atoms.set_constraint(cn)
-        atoms.rattle(rattle_std)
-        atoms.set_constraint()
-        atoms[31].position += distance * atoms.cell[0] / atoms.cell.lengths()[0]
-        atoms.wrap()
-    
+
     # Check if the relaxation has been done for the same structure
-    if ((outcar := direc/"vasp.out")).exists():
-        vasprun = direc/'vasprun.xml'
-        try:
-            # Check if the number of atoms in the vasprun is the same as the number of atoms in the atoms object
-            if len(atoms) == len(read(vasprun)):
-                # Check if the relaxation has been done
-                    outcar_txt = outcar.read_text()
-                    if "reached required accuracy - stopping structural energy minimisation" in outcar_txt:
-                        c.log(f"Relaxation for {name} has already been done")
-                        lets_run = False
-        except (ReadError, ParseError):
-            c.log("Error reading the vasprun.xml file. Running the relaxation again.")
+    # if ((outcar := direc/"vasp.out")).exists():
+    #     try:
+    #         # Check if the number of atoms in the vasprun is the same as the number of atoms in the atoms object
+    #         if len(atoms) == len(read(vasprun)):
+    #             # Check if the relaxation has been done
+    #                 outcar_txt = outcar.read_text()
+    #                 if "reached required accuracy - stopping structural energy minimisation" in outcar_txt:
+    #                     c.log(f"Relaxation for {name} has already been done")
+    #                     lets_run = False
+    #     except (ReadError, ParseError):
+    #         c.log("Error reading the vasprun.xml file. Running the relaxation again.")
             
     # Set up the calculator
+    
+    trajs_list = list(direc.glob("*relax.traj"))
+    trajs_list = [traj for traj in trajs_list if traj.is_file() and traj.stat().st_size > 0]
+    
+    vasprun = direc/'vasprun.xml'
+    if len(trajs_list) > 0:
+        last_traj = sorted(trajs_list, key=lambda x: int(x.stem.split("_")[-2]))[-1]
+        c.log(f"Attempting restart from trajectory: {last_traj.name}")
+        atoms = read(last_traj)
+        counter = int(last_traj.stem.split("_")[-2]) + 1
+        c.log(f"Restarting from step {counter}")
+    else:
+        counter = 0
+        cn = FixAtoms([31])
+        # If vasprun.xml is exists, try to read the last structure from it
+        if vasprun.exists():
+           try:
+               atoms = read(vasprun)
+               c.log("Read the last structure from vasprun.xml")
+           except (ReadError, ParseError):
+               c.log("Error reading the vasprun.xml file. Starting from the initial structure.")
+        else:
+            c.log("No vasprun.xml file found. Starting from the initial structure.")
+            atoms = read(direc/"POSCAR")
+            atoms.set_constraint(cn)
+            atoms.rattle(rattle_std)
+            atoms.set_constraint()
+            atoms[31].position += distance * atoms.cell[0] / atoms.cell.lengths()[0]
+            atoms.wrap()
+            c.log(f"Rattled the structure with std {rattle_std} and moved the dopant by {distance} along the x-axis")
+
+
     c.log(f"Setting up the calculator for {name}")
     calc = create_VASP_calc(atoms, 'PBEsol', direc.name, direc)
     set_magnetic_moments(atoms)
     vasp_settings = {
             'ibrion' : 2,
             'nsw' : 1,
-            'ediffg' : -0.05,
-            'nsw' : 250,
+            #'ediffg' : -0.05,
+            'nsw' : 0,
             'ncore' : ncore,
     }
     vasp_settings.update(vasp)
     calc.set(**vasp_settings)
     
     # Does the relaxation as well
-    if lets_run:
-        # Apply calculator to atoms
-        c.log(f"Relaxing {name}")
-        atoms.calc = calc
-        atoms.get_potential_energy()
-
+    #if lets_run:
+    # Apply calculator to atoms
+    c.log(f"Relaxing {name}")
+    atoms.calc = calc
+    atoms.get_potential_energy()
+    opt = FIRE(atoms, trajectory=f"{direc_string}/{name}_{counter}_relax.traj", logfile=f"{direc_string}/{name}_relax.log")
+    opt.run(fmax=0.05)
+    
     # Move the important files from scratch to home
     for file in direc.glob("*"):
         if file.is_file() and file.name != 'WAVECAR':
